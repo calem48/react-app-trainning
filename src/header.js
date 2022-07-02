e5717fbdfb03986bdcf3cd0d294968406a05e2f0
calem48 <messaoud.messaoud92@gmail.com>
2022-07-02 18:42:13 +0100

    add nav bar app

--- src/header.js
+++ src/header.js
@@ -1,5 +1,7 @@
 
 import React, { useRef, useState, useEffect } from 'react';
+import { navLinks } from "./data"
+
 
 const Header = () => {
     const [show, setshow] = useState(false);
@@ -7,30 +9,40 @@ const Header = () => {
     const nav = useRef(null)
 
     useEffect(() => {
-        let height = nav.current.getBoundingClientRect()
+        let height = nav.current.getBoundingClientRect().height
+        if (show) {
+            containerNav.current.style.height = height + "px"
 
-    }, []);
+        } else {
+            containerNav.current.style.height = 0 + "px"
+        }
+        if (containerNav.current.classList.contains("active")) {
+
+        }
+
+    }, [show]);
     return (
         <header>
             <div className="container">
                 <div className="logo">
-                    <a href="#">
+                    <a href="#home">
                         <img src="images/logo.png" alt="logo" />
                     </a>
-
                     <button onClick={() => setshow(!show)} className="mobile">
                         <i className="fa-solid fa-bars"></i>
                     </button>
                 </div>
-
-                <div className={`${show ? "nav active" : "nav"}`} ref={containerNav} >
+                {/* {`${show ? "nav active" : "nav"}`} */}
+                <div className="nav" ref={containerNav} >
                     <ul className="parent" ref={nav}>
-                        <li><a href="#home">home</a></li>
-                        <li><a href="#about">about</a></li>
-                        <li><a href="#services">services</a></li>
-                        <li><a href="#portfolio">portfolio</a></li>
-                        <li><a href="#team">team</a></li>
-                        <li><a href="#contact">contact</a></li>
+                        {
+                            navLinks.map(item => {
+                                const { id, url, text } = item
+                                return (
+                                    <li key={id}><a href={url} >{text}</a></li>
+                                )
+                            })
+                        }
                     </ul>
                 </div>
 
