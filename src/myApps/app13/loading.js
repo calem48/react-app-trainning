490b965f733ab6d35daa8360f0a52e24bcfa6fb1
calem48 <messaoud.messaoud92@gmail.com>
2022-07-13 12:36:28 +0100

    app14

--- src/myApps/app13/loading.js
+++ src/myApps/app13/loading.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Loading = () => {
-    return (
-        <h1 className='section-title'>Loading ...</h1>
-    );
+    return <h1>loading ...</h1>
 }
 
 export default Loading;
