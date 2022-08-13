5a0bb0807611f75d89fefab70243dd858382f8eb
calem48 <messaoud.messaoud92@gmail.com>
2022-08-13 16:42:59 +0100

    exo for redux toolkits

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header, Footer } from './compenent/index';
-import { HomePage, ErrorPage, SchoolDetails, SchoolPage, LoginPage } from './pages/index';
-function App() {
+import Navbar from './component/Navbar';
+import CartContainer from "./component/CartContainer"
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { cartTotalePrice, getItems } from './features/CartSlice';
+import Modal from './component/Modal';
 
+function App() {
+  const { cartItems, isLoading } = useSelector((state) => state.cart)
+  const { isOpen } = useSelector((state) => state.modal)
+
+  let dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(cartTotalePrice())
+  }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getItems("for test"))
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>loading ....</h1>
+      </div>
+    )
+  }
 
   return (
     <>
-      Hi
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
     </>
   );
-};
 
+};
 
 export default App;
